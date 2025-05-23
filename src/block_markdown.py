4dd6dcf06ac1fc8845449677a52bc4a5e7c5a43c
Nick Lanzo <nlanzo@gmail.com>
2025-05-23 15:10:42 -0500

    add functionality to convert markdown to html nodes

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from htmlnode import ParentNode
+from inline_markdown import text_to_textnodes
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 def markdown_to_blocks(markdown):
@@ -22,7 +25,7 @@ class BlockType(Enum):
 
 def block_to_block_type(markdown_block):
     lines = markdown_block.split("\n")
-    if markdown_block.startswith(("# ", "## ", "### ", "#### ", "##### ")):
+    if markdown_block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
         return BlockType.HEADING
     if markdown_block.startswith(("```")) and lines[-1].endswith("```"):
         return BlockType.CODE
@@ -42,3 +45,101 @@ def block_to_block_type(markdown_block):
                 return BlockType.PARAGRAPH
         return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
+        
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in blocks:
+        html_node = block_to_html_node(block)
+        children.append(html_node)
+    return ParentNode("div", children, None)
+
+
+def block_to_html_node(block):
+        block_type = block_to_block_type(block)
+        if block_type == BlockType.PARAGRAPH:
+            return paragraph_to_html_node(block)
+        if block_type == BlockType.HEADING:
+            return heading_to_html_node(block)
+        if block_type == BlockType.QUOTE:
+            return quote_to_html_node(block)
+        if block_type == BlockType.CODE:
+            return code_to_html_node(block)
+        if block_type == BlockType.UNORDERED_LIST:
+            return unordered_list_to_html_node(block)
+        if block_type == BlockType.ORDERED_LIST:
+            return ordered_list_to_html_node(block)
+        raise ValueError("Invalid block type")
+
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    children = []
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+    return children
+
+
+def paragraph_to_html_node(block):
+    lines = block.split("\n")
+    paragraph = " ".join(lines)
+    children = text_to_children(paragraph)
+    return ParentNode("p", children)
+
+
+def heading_to_html_node(block):
+    heading_level = 0
+    for char in block:
+        if char == "#":
+            heading_level += 1
+        else:
+            break
+    if heading_level + 1 >= len(block):
+        raise ValueError("invalid heading level")
+    text = block[heading_level + 1:]
+    children = text_to_children(text)
+    return ParentNode(f"h{heading_level}", children)
+
+
+def quote_to_html_node(block):
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        if not line.startswith(">"):
+            raise ValueError("invalid quote block, every line must begin with >")
+        new_lines.append(line.lstrip(">").strip())
+    content = " ".join(new_lines)
+    children = text_to_children(content)
+    return ParentNode("blockquote", children)
+    
+
+def code_to_html_node(block):
+    if not block.startswith("```") or not block.endswith("```"):
+        raise ValueError("invalid code block, must start and end with ```")
+    text = block[4:-3]
+    raw_text_node = TextNode(text, TextType.TEXT)
+    child = text_node_to_html_node(raw_text_node)
+    code = ParentNode("code", [child])
+    return ParentNode("pre", [code])
+
+
+def unordered_list_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[2:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ul", html_items)
+
+
+def ordered_list_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[3:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ol", html_items)
